13e74be4a50610472f0c90528b5aac511fa0fc9f
maimounakeita505-alt <maimounakeita505@gmail.com>
2026-02-26 06:44:07 -1100

    Update cv.py

--- cv.py
+++ cv.py
@@ -6,18 +6,19 @@ with st.sidebar:
     st.write("Email: maimounakeita505@gmail.com")
     st.write("Tel: 776483089")
     st.write("Adresse: BOPP,rue 1, Villa 100")
-    st.write("FORMATIONS")
-    st.write("2024: BAC L2")
-    st.write("2026: BTS en Geomatique")
 
 st.header("Technicienne supérieure en Géomatique")
 
 st.subheader("Description professionnel")
 st.write("Etudiante en Géomatique, Passionnée par la Photogrammétrie, La télédetection et la Cartographie, je suis une personne ambitieuse dotée d'une capacité à etre attentive aux details pour mener à bien mes projets")
+    
+st.subheader("FORMATIONS")
+st.write("2024: BAC L2")
+st.write("2026: BTS en Geomatique") 
 
 st.subheader("Projets académiques")
 st.write("Conception de bases de données géographiques")
-st.write("Réalisation d’analyses spatiales (buffer, intersection, superposition")
+st.write("Réalisation d’analyses spatiales (buffer, intersection, superposition"))
 st.write("Traitement de données topographiques")
 st.write("Traitement d'images dans Agisoft et Pix4D Capture")
 st.write("Projet Intégrateur")
@@ -26,7 +27,7 @@ st.subheader("Compétences")
 st.write("Conception de base de données sur QUANTUM GIS et ARCGIS")
 st.write("Concevoir une base de donnée sur POWER AMC, Post gres")
 st.write("Concevoir des cartes thématiques, cartes de situations, cartes climatiques,etc")
-st.write("Géoreferencement de carte")
+st.write("Géoreferencement")
 st.write("Numérisation")
 st.write("Maitrise des techniques de collecte données: application: Qfiel, Locus Map, Mobile Topo, Kobo Toolbox")
 st.write("Conception  de Plans sur Autocad et Sketchup")
@@ -35,6 +36,7 @@ st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données
 st.subheader("Objectifs générales")
 st.write("Mettre mes compétences en analyse spatiale au service des projets environnementaux, urbains et territoriaux afin d’aider à la prise de décision.")
 st.write("Specialiste Géomatique")
+
 
 
 
