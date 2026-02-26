c57db171ff05ab86709a60ec7d60d3f76413c655
maimounakeita505-alt <maimounakeita505@gmail.com>
2026-02-26 06:07:54 -1100

    Update cv.py

--- cv.py
+++ cv.py
@@ -3,42 +3,38 @@ import streamlit as st
 with st.sidebar:
     st.write("Sokhna Maimouna Keita")
     st.write("CONTACTS")
-    st.write("Email: dpapademba59@gmail.com")
-    st.write("Tel: 78147739")
-    st.write("Adresse: Medina,rue 06")
+    st.write("Email: maimounakeita505@gmail.com")
+    st.write("Tel: 776483089")
+    st.write("Adresse: BOPP,rue 1, Villa 100")
     st.write("FORMATIONS")
-    st.write("2021-2022: BAC L2")
-    st.write("2022-2025: Licence en Geographie")
-    st.write("2025-2026:BTS en Geomatique")
+    st.write("2024: BAC L2")
+    st.write("2026: BTS en Geomatique")
 
-st.header("Technicien supérieur en Geomatique")
+st.header("Technicienne supérieure en Géomatique")
 
 st.subheader("A propos de moi")
-st.write("Passionné par la Geographie, la Géomatique et la Cartographie, je suis une personne rigoureuse dotée d'une capacité de travailler en equipe pour mener à bien des projets")
+st.write("Etudiante en Géomatique, Passionnée par la Photogrammétrie, La télédetection et la Cartographie, je suis une personne ambitieuse dotée d'une capacité à etre attentive aux details pour mener à bien mes projets")
 
 st.subheader("Projets académiques")
-st.write("Programmation: Réalisation d'un CV digital sur Steamlit")
-st.write("Projection cartographique: Collecte de données aux environs de la Mosquée Masalik djinane")
-st.write("Base de données: Georeferencement d'une carte, création d'une base de données spatiales")
-st.write("Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)")
-
-st.subheader("Expériences")
-st.write("Poste d'adjoint president de la communication de l'amical des etudiants thièsois")
-st.write("Poste de commissaire au compte dans un GIE nommé OJEUF")
-
+st.write("Conception de bases de données géographiques")
+st.write("Réalisation d’analyses spatiales (buffer, intersection, superposition")
+st.write("Traitement de données topographiques")
+st.write("Traitement d'images dans Agisoft et Pix4D Capture")
+st.write("Projet Intégrateur")
 
 st.subheader("Compétences")
 st.write("Conception de base de données sur QUANTUM GIS et ARCGIS")
 st.write("Concevoir une base de donnée sur POWER AMC, Post gres")
+st.write("Concevoir des cartes thématiques, cartes de situations, cartes climatiques,etc")
 st.write("Géoreferencement de carte")
 st.write("Numérisation")
-st.write("Maitrise des techniques de collecte données:application Mobile Topographer,Kobo Toolbox")
-st.write("Conception  de Plans sur Autocad")
-st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets")
+st.write("Maitrise des techniques de collecte données: application: Qfiel, Locus Map, Mobile Topo, Kobo Toolbox")
+st.write("Conception  de Plans sur Autocad et Sketchup")
+st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets académiques")
 
-st.subheader("Perspectives")
-st.write("Ambitionne de créer un cabinet d'expertise en Géomatique spécialisé dans L'Amenagement du territoire")
-st.write("Ambitionne de concevoir et d'administrer des bases de données spatiales (post gis) pour l'analyse de données geographiques")
-st.write("Specialiste SiG")
+st.subheader("Objectifs générales")
+st.write("Mettre mes compétences en analyse spatiale au service des projets environnementaux, urbains et territoriaux afin d’aider à la prise de décision.")
+st.write("Specialiste Géomatique")
+
 
 
