47addd11f5217e8a00f1bb3c51a331533ed3a372
maimounakeita505-alt <maimounakeita505@gmail.com>
2026-02-27 04:04:46 -1100

    Update cv.py

--- cv.py
+++ cv.py
@@ -4,12 +4,13 @@ with st.sidebar:
     st.write("CONTACTS")
     st.write("Email: maimounakeita505@gmail.com")
     st.write("Adresse: BOPP, rue 1, Villa 100")
+    st.divider()
+    st.markdown("## FORMATIONS")
+    st.markdown("""
+    _2024: Baccalauréat L2
+    _2026: BTS en Géomatique
+    """)
 
-st.markdown("## FORMATIONS")
-st.markdown("""
-_2024: Baccalauréat L2
-_2026: BTS en Géomatique
-""")
 
 st.header("Sokhna Maimouna Keita")
 
@@ -35,6 +36,7 @@ st.write("*Maitrise avancée du Pack Office (EXCEL pour le traitement de donnée
 st.subheader("Objectifs générales")
 st.write("Mettre mes compétences en analyse spatiale au service des projets environnementaux, urbains et territoriaux afin d’aider à la prise de décision.")
 st.write("Specialiste Géomatique")
+
 
 
 
