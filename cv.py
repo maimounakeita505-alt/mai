e711fca745d011b08cd86dfea9b84ae335174b03
maimounakeita505-alt <maimounakeita505@gmail.com>
2026-02-27 02:28:20 -1100

    Update cv.py

--- cv.py
+++ cv.py
@@ -4,8 +4,7 @@ with st.sidebar:
     st.write("Sokhna Maimouna Keita")
     st.write("CONTACTS")
     st.write("Email: maimounakeita505@gmail.com")
-    st.write("Tel: 776483089")
-    st.write("Adresse: BOPP,rue 1, Villa 100")
+    st.write("Adresse: BOPP, rue 1, Villa 100")
 
 st.header("curriculum Vitae")
 
@@ -13,29 +12,29 @@ st.subheader("Profil professionnel")
 st.write("Etudiante en Géomatique, Passionnée par la Photogrammétrie, la télédetection et la Cartographie, je suis une personne ambitieuse dotée d'une capacité à être attentive aux details pour mener à bien mes projets")
     
 st.subheader("FORMATIONS")
-st.write("2024: BAC L2")
-st.write("2026: BTS en Geomatique") 
+st.write("*2024: BAC L2")
+st.write("*2026: BTS en Geomatique") 
 
 st.subheader("Projets académiques")
-st.write("*Concevoir des bases de données géographiques")
-st.write("*Réaliser des analyses spatiales (buffer, intersection, superposition)")
-st.write("*Traiter des données topographiques")
-st.write("*Traiter des images dans Agisoft et Pix4D Capture")
-st.write("*Planifier le Projet Intégrateur")
+st.write("*Conception de bases de données géographiques")
+st.write("*Réalisation d'analyses spatiales (buffer, intersection, superposition)")
+st.write("*Traitement de données topographiques")
+st.write("*Traitement des images dans Agisoft et Pix4D Capture")
+st.write("*Planification du  Projet Intégrateur")
 
 st.subheader("Compétences")
-st.write("Conception de base de données sur QUANTUM GIS et ARCGIS")
-st.write("Concevoir une base de donnée sur POWER AMC, Post gres")
-st.write("Concevoir des cartes thématiques, cartes de situations, cartes climatiques,etc")
-st.write("Géoreferencement")
-st.write("Numérisation")
-st.write("Maitrise des techniques de collecte données: application: Qfiel, Locus Map, Mobile Topo, Kobo Toolbox")
-st.write("Conception  de Plans sur Autocad et Sketchup")
-st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets académiques")
+st.write("*Concevoir des cartes thématiques, cartes de situations, cartes climatiques, etc")
+st.write("*Concevoir des base de données sur QUANTUM GIS et ARCGIS")
+st.write("*Conception de Plans sur Autocad et Sketchup")
+st.write("*Géoreferencement")
+st.write("*Numérisation")
+st.write("*Maitrise des techniques de collecte données: application: Qfiel, Locus Map, Mobile Topo, Kobo Toolbox")
+st.write("*Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets académiques")
 
 st.subheader("Objectifs générales")
 st.write("Mettre mes compétences en analyse spatiale au service des projets environnementaux, urbains et territoriaux afin d’aider à la prise de décision.")
 st.write("Specialiste Géomatique")
+
 
 
 
